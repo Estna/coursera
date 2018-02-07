2aeae47199572569c6806f583c02f2d95b6201ef
Estna <estna@coursera-smth.com>
2018-02-08 01:28:50 +0200

    'module3'

---
+++ module3-solution/app.js
@@ -0,0 +1,13 @@
+(function(){
+'use strict';
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService);
+
+NarrowItDownController.$inject['MenuSearchService'];
+
+function NarrowItDownController(MenuSearchService){
+
+}
+
+});
